272a0b7b7240fdbf23d883ceb6db4a92d9587246
Almudena <almudena.zhou.rl@gmail.com>
2024-10-27 18:57:44 +0100

    Bugfix: get the ollama_url in the ollama client from the OLLAMA_URL env variable

--- src/client_modules/llms/openai/ollama.py
+++ src/client_modules/llms/openai/ollama.py
@@ -10,11 +10,11 @@ logger = logging.getLogger(__name__)
 
 class OllamaClient(BaseOpenAIClient):
 
-    def __init__(self, model_name=os.getenv("OLLAMA_MODEL")) -> None:
+    def __init__(self, model_name=os.getenv("OLLAMA_MODEL"), ollama_url=os.getenv("OLLAMA_URL")) -> None:
         super().__init__()
-        
+
         self.client = OpenAI(
-                base_url='http://localhost:11434/v1/',
+                base_url=ollama_url,
                 api_key="ollama",
             )
             
